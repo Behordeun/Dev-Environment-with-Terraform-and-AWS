d555ce0dca41025cc8d8512182505e20b1f38d78
Muhammad Abiodun SULAIMAN <prince.behordeun@gmail.com>
2022-03-07 15:03:43 +0100

    Formated the 'main.tf' and 'providers.tf' files respectively

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "aws_vpc" "devops-mtc-vpc" {
-  cidr_block = "10.123.0.0/16"
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
     Name = "dev"
   }
 }
 
 resource "aws_subnet" "devops-mtc-subnet" {
-  vpc_id     = aws_vpc.devops-mtc-vpc.id
-  cidr_block = "10.123.1.0/24"
+  vpc_id                  = aws_vpc.devops-mtc-vpc.id
+  cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "devops-subnet"
